c163fd12faa5c8b92cc7cac917e092e18603a0e4
zhangyuanlu <zhangyuanlu327@gmail.com>
2018-08-22 23:10:48 +0800

    add layout for chapters list

---
+++ app/src/main/java/com/zyl/kuaikan/ChapterList/ChapterListActivity.java
@@ -0,0 +1,15 @@
+package com.zyl.kuaikan.ChapterList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zyl.kuaikan.R;
+
+public class ChapterListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter_list);
+    }
+}
